b1015410a5dc7fa7d8aa357ca943657e9c9d6ba1
Tsvetan <tsvetan_sabotinov@yahoo.com>
2020-12-07 12:26:30 +0200

    Move "nav" component from "user" module to "my common" module.

--- src/app/modules/commonM/common.module.ts
+++ src/app/modules/commonM/common.module.ts
@@ -4,12 +4,13 @@ import { AboutComponent } from './aboutC/about.component';
 import { ErrorComponent } from './errorC/error.component';
 import { FooterComponent } from './footerC/footer.component';
 import { UnderconstructionComponent } from './underconstructionC/underconstruction.component';
+import { NavComponent } from './navC/nav.component';
 
 
 
 @NgModule({
-  declarations: [AboutComponent, ErrorComponent, FooterComponent, UnderconstructionComponent],
-  exports: [AboutComponent, ErrorComponent, FooterComponent, UnderconstructionComponent],
+  declarations: [AboutComponent, ErrorComponent, FooterComponent, UnderconstructionComponent, NavComponent],
+  exports: [AboutComponent, ErrorComponent, FooterComponent, UnderconstructionComponent, NavComponent],
   imports: [
     CommonModule
   ]
--- src/app/modules/userM/user.module.ts
+++ src/app/modules/userM/user.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './homeC/home.component';
-import { NavComponent } from './navC/nav.component';
 import { ShowComponent } from './showC/show.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, NavComponent, ShowComponent],
-  exports: [HomeComponent, NavComponent, ShowComponent],
+  declarations: [HomeComponent, ShowComponent],
+  exports: [HomeComponent, ShowComponent],
   imports: [
     CommonModule
   ]
